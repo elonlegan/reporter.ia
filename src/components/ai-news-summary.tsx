f9bd2deaa994cdae98f5c0a36b98cdb055c29d59
Angel Leon <angel.leon@tekus.co>
2024-08-04 00:51:42 -0500

    ready style

--- src/components/ai-news-summary.tsx
+++ src/components/ai-news-summary.tsx
@@ -4,11 +4,11 @@ export function AINewsSummary({
 	summary: string;
 }) {
 	return (
-		<div className='flex flex-wrap w-full h-full'>
+		<div className='flex flex-wrap w-full h-full justify-between'>
 			<p className='w-11/12 first-letter:text-7xl first-letter:font-bold first-letter:mr-3 first-letter:float-left first-letter:leading-none  text-justify'>
 				{summary}
 			</p>
-			<div className='bg-black w-1 h-11/12 rounded ml-4'></div>
+			<div className='bg-black w-1 h-11/12 rounded '></div>
 			<div className='bg-black w-full h-1 rounded my-3'></div>
 		</div>
 	);
